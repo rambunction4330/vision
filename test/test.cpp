e0aa4e7a87037a87b84ea9149ecbb28d783cca9d
Sithija <root@debian>
2016-01-29 15:42:25 -0600

    new file:   test/test 	new file:   test/test.cpp 	new file:   test/test.cpp.save 	new file:   test/test.cpp.save.1

---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	cout << "deez nuts" << endl;
+	return 0;
+}
